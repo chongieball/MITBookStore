32d343c28454148dc0f699d9ae0454df250e2a7f
chongieball <chongg1243@gmail.com>
2017-03-24 06:05:29 +0700

    update route

--- app/route.php
+++ app/route.php
@@ -12,10 +12,10 @@ $app->get('/', $namespace. '\HomeController:index')->setName('home')->add(new Sh
 $app->get('/admin/login', $namespace.'\AdminController:getSignIn')->setName('admin.login');
 $app->post('/admin/login', $namespace.'\AdminController:postSignIn');
 
+$app->get('/admin/logout', $namespace.'\AdminController:Logout')->setName('admin.logout');
+
 $app->group('/admin', function () use ($app,$namespace) {
 	$app->get('', $namespace.'\AdminController:index')->setName('admin.index');
-
-	$app->get('/logout', $namespace.'\AdminController:Logout')->setName('admin.logout');
 
 	$app->get('/change_password', $namespace. '\AdminController:getChangePassword')->setName('admin.change_password');
 
@@ -101,32 +101,11 @@ $app->group('/admin', function () use ($app,$namespace) {
 	$app->get('/author/update/{id}', $namespace.'\AuthorController:getUpdate')->setName('author.update');
 
 	$app->post('/author/postupdate/{id}', $namespace.'\AuthorController:postUpdate')->setName('author.postupdate');
-})->add(new AdminMiddleware($container));
-
-//-------------------FOR USER
-$app->get('/register', $namespace.'\UserController:getRegister')->setName('user.register');
-$app->post('/register', $namespace.'\UserController:postRegister');
-
-$app->get('/login', $namespace. '\UserController:getLogin')->setName('user.login');
-$app->post('/login', 'MBS\Controllers\UserController:postLogin');
-
-$app->get('/reset_password', $namespace. '\UserController:getResetPassword')->setName('user.reset_password');
-$app->post('/reset_password', $namespace. '\UserController:postResetPassword');
-
-$app->get('/cart', $namespace. '\CartController:index')->setName('cart.index');
-$app->post('/cart', $namespace. '\CartController:add');
-
-//user must login before access this route
-$app->group('', function () use ($app, $namespace) {
-	$app->get('/logout', $namespace. '\UserController:getLogout')->setName('user.logout');
-	$app->get('/edit', $namespace. '\UserController:getEdit')->setName('user.edit');
-	$app->post('/edit', $namespace. '\UserController:postEdit')->setName('user.edit');
-	$app->get('/my_account', $namespace. '\UserController:getAccount')->setName('user.account');
 // -----------------------------------------------------------------------------
 	$app->get('/publisher', $namespace.'\PublisherController:index')
     ->setName('publisher.index');
 
-	$app->get('/publisher/', $namespace.'\PublisherController:index')
+	$app->get('/publisher', $namespace.'\PublisherController:index')
         ->setName('publisher.index');
 
 	$app->get('/publisher/read/{term}', $namespace.'\PublisherController:readAjax')
@@ -153,7 +132,27 @@ $app->group('', function () use ($app, $namespace) {
 
 	$app->get('/publisher/update/{id}', $namespace.'\PublisherController:getUpdate')
         ->setName('publisher.update');
-
 	$app->post('/publisher/postupdate/{id}', $namespace.'\PublisherController:postUpdate')
         ->setName('publisher.postupdate');
+})->add(new AdminMiddleware($container));
+
+//-------------------FOR USER
+$app->get('/register', $namespace.'\UserController:getRegister')->setName('user.register');
+$app->post('/register', $namespace.'\UserController:postRegister');
+
+$app->get('/login', $namespace. '\UserController:getLogin')->setName('user.login');
+$app->post('/login', 'MBS\Controllers\UserController:postLogin');
+
+$app->get('/reset_password', $namespace. '\UserController:getResetPassword')->setName('user.reset_password');
+$app->post('/reset_password', $namespace. '\UserController:postResetPassword');
+
+$app->get('/cart', $namespace. '\CartController:index')->setName('cart.index');
+$app->post('/cart', $namespace. '\CartController:add');
+
+//user must login before access this route
+$app->group('', function () use ($app, $namespace) {
+	$app->get('/logout', $namespace. '\UserController:getLogout')->setName('user.logout');
+	$app->get('/edit', $namespace. '\UserController:getEdit')->setName('user.edit');
+	$app->post('/edit', $namespace. '\UserController:postEdit')->setName('user.edit');
+	$app->get('/my_account', $namespace. '\UserController:getAccount')->setName('user.account');
 })->add(new UserMiddleware($container));
