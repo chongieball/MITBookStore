c8fbdcab39554f3ff117f47c7173bfc42cccdaf6
chongieball <chongg1243@gmail.com>
2017-03-30 14:51:30 +0700

    new skeleton

--- app/container.php
+++ app/container.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
 
 use Slim\Container;
 use Slim\Views\Twig as View;
 use Slim\Views\TwigExtension as ViewExt;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local;
 
 $container = $app->getContainer();
 
-$container['db'] = function (Container $container) use ($setting) {
+$container['db'] = function (Container $container) {
 	$setting = $container->get('settings');
 
 	$config = new \Doctrine\DBAL\Configuration();
@@ -14,10 +16,10 @@ $container['db'] = function (Container $container) use ($setting) {
 	$connect = \Doctrine\DBAL\DriverManager::getConnection($setting['db'],
 		$config);
 
-	return $connect->createQueryBuilder();
+	return $connect;
 };
 
-$container['view'] = function (Container $container) use ($setting) {
+$container['view'] = function (Container $container) {
 	$setting = $container->get('settings')['view'];
 
 	$view = new View($setting['path'], $setting['twig']);
@@ -25,22 +27,65 @@ $container['view'] = function (Container $container) use ($setting) {
 
 	$view->getEnvironment()->addGlobal('flash', $container->flash);
 
-	$view->getEnvironment()->addGlobal('old', $_SESSION['old']);
-	unset($_SESSION['old']);
+	if ($_SESSION['old']) {
+		$view->getEnvironment()->addGlobal('old', $_SESSION['old']);
+		unset($_SESSION['old']);
+	}
 
-	$view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
-	unset($_SESSION['errors']);
+	if ($_SESSION['errors']) {
+		$view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+		unset($_SESSION['errors']);
+	}
 	
+	if ($_SESSION['cart']) {
+		$view->getEnvironment()->addGlobal('cart', $_SESSION['cart']);
+	}
+
+	if ($_SESSION['user']) {
+		$view->getEnvironment()->addGlobal('user', $_SESSION['user']);
+	}
+	
+	$view->getEnvironment()->addGlobal('basket', $container->basket);
+
+	if (isset($_SESSION['old'])) {
+		$view->getEnvironment()->addGlobal('old', $_SESSION['old']);
+		unset($_SESSION['old']);
+	}
+
+	if (isset($_SESSION['errors'])) {
+		$view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+		unset($_SESSION['errors']);
+	}
 	return $view;
 };
 
-$container['validator'] = function (Container $container) use ($setting) {
+$container['validator'] = function (Container $container) {
 	$setting = $container->get('settings')['lang']['default'];
 	$params = $container['request']->getParams();
-	
+
 	return new Valitron\Validator($params, [], $setting);
 };
 
 $container['flash'] = function (Container $container) {
 	return new \Slim\Flash\Messages;
 };
+
+$container['csrf'] = function (Container $container) {
+	return new \Slim\Csrf\Guard;
+};
+
+$container['session'] = function (Container $container) {
+	return new MBS\Core\Storage\SessionStorage;
+};
+
+$container['basket'] = function (Container $container) {
+	return new MBS\Basket\Basket(
+		$container->session, 
+		new MBS\Models\Book($container->db)
+	);
+};
+
+$container['upload'] = function (Container $container) {
+	$setting = $container->get('settings')['uploadPath'];
+	return new \Upload\Storage\FileSystem($setting);
+}; 
